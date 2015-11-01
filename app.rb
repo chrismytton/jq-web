18f0d072b92e9647f08cf9ef18341e4ef2a79c63
Chris Mytton <chrismytton@gmail.com>
2015-11-01 18:07:14 +0000

    Better layout

--- app.rb
+++ app.rb
@@ -43,36 +43,3 @@ end
     end
   end
 end
-
-__END__
-
-@@index
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Create an API for static JSON files">
-    <meta name="author" content="https://www.chrismytton.uk">
-    <title>jq(1) for the web</title>
-    <style>
-      body {
-        max-width: 600px;
-        margin: 0 auto;
-        padding: 10px;
-        color: #444;
-        font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
-      }
-    </style>
-  </head>
-  <body>
-    <h1>jq(1) for the web</h1>
-
-    <form method="get" action="<%= url('/jq') %>">
-      <input type="url" name="json">
-      <input type="text" name="filter" placeholder=".[]|.name">
-      <input type="submit" value="Query">
-    </form>
-  </body>
-</html>
